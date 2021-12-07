b3f1a873d1af63df210575725b5f199a6fe426c5
Emmanuel DAVEAU <emmanuel@high-stickers.com>
2021-12-07 17:01:22 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # lire toutes les tâches
+  get 'tasks', to: 'tasks#index'
+
+  #lire 1 seule tâches
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+
+  patch 'tasks/:id', to: 'tasks#update'
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(title: 'chaussons', details: 'Aux pommes!')
+Task.create(title: 'Suivi', details: 'de colis', completed: true)
